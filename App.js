46aa6d48befbfec5d9617b662c1bca505d754a55
Sergey Germanov <husky@Sergeys-MacBook-Pro.local>
2023-02-13 19:40:32 +1300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Hello World!!!</Text>
+      </View>      
+      <Text>Happy to see you! Yo!</Text>
+      <Button title='Click me!'/>
     </View>
+    
   );
 }
 
